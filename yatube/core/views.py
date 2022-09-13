91bcd2eaf30b285520d72e1ca6b2bab9b0ae8efd
Дмитрий Венде <vende.d@mail.ru>
2022-09-13 21:28:23 +0300

    z

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -9,9 +9,9 @@ def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
 
 
-def forbidden(request, exception):
-    return render(request, 'core/403.html', {'path': request.path}, status=403)
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
 
 
-def internal_server_error(request, exception):
-    return render(request, 'core/500.html', {'path': request.path}, status=500)
+def permission_denied(request, exception):
+    return render(request, 'core/403.html', status=403)
