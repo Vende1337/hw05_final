f95948b3f86da00c68c83af5e0d954783a03a12e
Дмитрий Венде <vende.d@mail.ru>
2022-09-16 21:03:21 +0300

    z

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -37,11 +37,11 @@ def profile(request, username):
     author = get_object_or_404(User, username=username)
     page_obj = get_page(author.posts.select_related(
         'group'), request.GET.get('page'))
-    following = request.user.is_authenticated and not (
+    following = request.user.is_authenticated and (
         Follow.objects.filter(
             user__follower__author=author,
             author__following__user=request.user)
-        .exists() and request.user == author)
+        .exists() and request.user != author)
     context = {
         'author': author,
         'page_obj': page_obj,
